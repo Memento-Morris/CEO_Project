81be423031d268b16cc15f3e63f8138f427dea4d
Morris <99559527+Memento-Morris@users.noreply.github.com>
2026-02-10 14:08:42 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+why doesntit go to the top of the page when i press next or go back.import streamlit as st
 import datetime
 import time
 import math
@@ -1340,4 +1340,3 @@ elif st.session_state.page == 3:
     show_page_3()
 elif st.session_state.page == 4:
     show_page_4()
-
